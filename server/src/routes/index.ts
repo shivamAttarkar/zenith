65aaf0bc25a96698419bfb116373006e3ab7ac84
Shivam Attarkar <shivamattarkar89@gmail.com>
2026-03-22 08:18:32 +0530

    added routes versioning

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,7 +3,7 @@ import { publicKeyRoutes } from "./public-key";
 import { friendRequestRoutes } from "./friend-request";
 import { userRoutes } from "./user";
 
-export const routes = new Elysia({ prefix: "/api" })
+export const routes = new Elysia({ prefix: "/api/v1" })
   .use(publicKeyRoutes)
   .use(friendRequestRoutes)
   .use(userRoutes);
