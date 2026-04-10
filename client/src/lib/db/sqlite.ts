b7ffabd82e21c2bc76dd6c54755cac0dd164901a
Shivam Attarkar <shivamattarkar89@gmail.com>
2026-04-10 21:42:33 +0530

    Update sqlite.ts

--- client/src/lib/db/sqlite.ts
+++ client/src/lib/db/sqlite.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import Database from '@tauri-apps/plugin-sql';
 import { drizzle } from 'drizzle-orm/sqlite-proxy';
 import * as schema from './schema';
@@ -12,28 +13,19 @@ function isSelectQuery(sql: string): boolean {
 
 export const sqlite = drizzle<typeof schema>(
   async (sql, params, method) => {
-    let rows: any = [];
-    let results = [];
-
-    if (isSelectQuery(sql)) {
-      rows = await db.select(sql, params).catch((e) => {
-        console.error('SQL Error:', e);
-        return [];
-      });
-    } else {
-      rows = await db.execute(sql, params).catch((e) => {
-        console.error('SQL Error:', e);
-        return [];
-      });
+    if (!isSelectQuery(sql)) {
+      await db.execute(sql, params).catch((e) => console.error('SQL Error:', e));
       return { rows: [] };
     }
 
-    rows = rows.map((row: any) => {
-      return Object.values(row);
-    });
+    const rows = (await db.select(sql, params).catch((e) => {
+      console.error('SQL Error:', e);
+      return [];
+    })) as any[];
 
-    results = method === 'all' ? rows : rows[0];
+    const mapped = rows.map((row: any) => Object.values(row));
+    const results = method === 'all' ? mapped : mapped[0];
     return { rows: results };
   },
-  { schema: schema, logger: true }
+  { schema: schema }
 );
