014cc572fbc5cc8511c76b9929ab25e9db85aea5
Shivam Attarkar <shivamattarkar89@gmail.com>
2026-06-13 09:22:25 +0530

    added icons and delay to show the main window

--- client/src-tauri/src/lib.rs
+++ client/src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::Manager;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,6 +9,15 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+            // added delay to wait for webview to load
+            std::thread::spawn(move || {
+                std::thread::sleep(std::time::Duration::from_millis(4000));
+                let _ = window.show();
+            });
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
